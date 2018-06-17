8a8eba12ae769bbd712603102f9012a4597a8916
DingJi <dingji97@outlook.com>
2018-06-17 23:55:07 +0800

    easy BST

---
+++ Row.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> q = new Queue<TreeNode>;
+        q.offer(root);
+        while (!q.isEmpty()){
+
+        }
+    }
+}
