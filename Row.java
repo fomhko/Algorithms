3d45c64ad80057837e79fc39dec752652532de3d
DingJi <dingji97@outlook.com>
2018-06-18 00:12:34 +0800

    Queue usage and MIN_VALUE

--- Row.java
+++ Row.java
@@ -9,10 +9,24 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        Queue<TreeNode> q = new Queue<TreeNode>;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<Integer> v = new ArrayList<Integer>();
+        if(root == null) return v;
         q.offer(root);
         while (!q.isEmpty()){
-
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            while(size > 0){
+                size--;
+                TreeNode n = q.poll();
+                max = Math.max(max,n.val);
+                if(n.left != null)
+                    q.offer(n.left);
+                if(n.right != null)
+                    q.offer(n.right);
+            }
+            v.add(max);
         }
+        return v;
     }
 }
