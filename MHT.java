f9bf3938238f334f7206de3fe7f4f6915362873e
DingJi <dingji97@outlook.com>
2018-06-19 23:23:08 -0800

    linked list

--- MHT.java
+++ MHT.java
@@ -1,19 +1,22 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        int[][] matrix = new int[n][n];
+        List<List<Integer>> myGraph = new ArrayList<List<Integer>>();
         int[] visited = new int[n];
         int[] dist = new int[n];
-        for (int[] i : edges){
-            matrix[i[0]][i[1]] = 1;
-            matrix[i[1]][i[0]] = 1;
+        for(int i=0; i<n; i++) {
+    	    myGraph.add(new ArrayList<Integer>());
+        }
+        for(int i=0; i<edges.length; i++) {
+            myGraph.get(edges[i][0]).add(edges[i][1]);
+            myGraph.get(edges[i][1]).add(edges[i][0]);
         }
         Queue<Integer> q = new LinkedList<Integer>();
         q.offer(0);
         visited[0] = 1;
         while(!q.isEmpty()){
             int tmp = q.poll();
-            for(int i = 0; i < n; i++){
-                if(matrix[tmp][i] == 1 && visited[i] != 1){
+            for (int i : myGraph.get(tmp)){
+                if(visited[i] != 1){
                     visited[i] = 1;
                     q.offer(i);
                     dist[i] = dist[tmp] + 1;
@@ -36,35 +39,48 @@ class Solution {
         visited[maxidx] = 1;
         while(!q.isEmpty()){
             int tmp = q.poll();
-
-            for (int i = 0; i < n; i++){
-                if(matrix[tmp][i] == 1 && visited[i] != 1){
+            for (int i : myGraph.get(tmp)){
+                if(visited[i] != 1){
                     visited[i] = 1;
                     q.offer(i);
                     dist[i] = dist[tmp]+1;
                 }
             }
         }
-        for (int i : dist)
-            System.out.println(i);
 
-        max = 0;
+        max = 0;maxidx = 0;
         for(int i = 0; i < n;i++){
             if (dist[i] > max){
                 max = dist[i];
+                maxidx = i;
             }
         }
-
+        int[] revdist = new int[n];
+        revdist[maxidx] = 0;
+        q.offer(maxidx);
+        visited = null;
+        visited = new int[n];
+        visited[maxidx] = 1;
+        while(!q.isEmpty()){
+            int tmp = q.poll();
+            for (int i : myGraph.get(tmp)){
+                if(visited[i] != 1){
+                    visited[i] = 1;
+                    q.offer(i);
+                    revdist[i] = revdist[tmp]+1;
+                }
+            }
+        }
 
         List<Integer> res = new ArrayList<Integer>();
         for (int i = 0; i < n;i++){
             if(max%2!=0){
-                if(dist[i] == max/2 || dist[i] == max/2+1){
+                if((dist[i] == max/2 || dist[i] == max/2+1) && (revdist[i] == max/2 || revdist[i] == max/2+1)){
                     res.add(i);
                 }
             }
             else{
-                if(dist[i] == max/2){
+                if(dist[i] == max/2 && revdist[i] == max/2){
                     res.add(i);
                 }
             }
