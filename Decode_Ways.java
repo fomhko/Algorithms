18b9acf108dd292678fb13b3f32d1590d7ea5ef8
DingJi <dingji97@outlook.com>
2018-06-21 22:55:22 +0800

    Decode_Ways.java

--- Decode_Ways.java
+++ Decode_Ways.java
@@ -1,18 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
-        func(s,0);
+        return func(s,0);
     }
     private int func(String s,int pos){
-        if(pos == s.length()-1){
-          return 1;
+        if(s.length() == 0){
+            return 0;
         }
-        if(pos >= s.length()){
-          return 0;
+        if(pos >= s.length()-1){
+            if(pos == s.length() - 1 && s.charAt(pos) == '0'){
+                return 0;
+            }
+            return 1;
         }
-        int result;
-        if(s.charAt(pos) < '2' ||(s.charAt(pos) == '2' && s.charAt(pos+1) <= '6')){
-          result += func(pos+2);
+        if(s.charAt(pos) == '0'){
+            return 0;
         }
-        result += func(pos+1);
+        int result = 0;
+        if((s.charAt(pos) < '2' ||(s.charAt(pos) == '2' && s.charAt(pos+1) <= '6')) && s.charAt(pos) > '0'){
+          result += func(s,pos+2);
+        }
+        result += func(s,pos+1);
+        return result;
     }
 }
