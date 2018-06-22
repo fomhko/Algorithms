12e79ac830cf372820f030724507fccec989d672
DingJi <dingji97@outlook.com>
2018-06-22 14:35:37 +0800

    submit

--- jump_game.java
+++ jump_game.java
@@ -6,17 +6,17 @@ class Solution {
                 break;
             }
             int max = Integer.MIN_VALUE;
-            int maxidx = index;
+            int maxidx = 0;
             for (int i = 1;i <= nums[index] && i+index < nums.length;i++){
-                if(nums[i+index] > max){
-                    max = nums[i+index];
-                    maxidx = i+index;
+                if(i+nums[i+index] > max){
+                    max = i+nums[i+index];
+                    maxidx =i;
                 }
             }
-            index = maxidx;
+            index += maxidx;
         }
         // System.out.println(nums.length);
-        if(index == nums.length - 1){
+        if(index >= nums.length - 1){
             return true;
         }
         return false;
